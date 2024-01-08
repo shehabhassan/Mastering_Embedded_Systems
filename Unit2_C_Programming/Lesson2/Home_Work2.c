a1e7f48e5b9fe577698dec96dec7c833cb609dd2
Shehab Hassan <shehabshwki@gmail.com>
2024-01-08 19:02:35 +0200

    Lesson 2 Homework2

---
+++ Unit2_C_Programming/Lesson2/Home_Work2.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+int main()
+{
+// EX1: Write a C program to check whether a number is even or odd.
+//    int num;
+//    printf("Enter a number: ");
+//    scanf("%d", &num); 
+//    num%2==0 ? printf("%d is Even", num) : printf("%d is Odd", num);
+   // EX2: Write a C program to check whether a char is vowel or consonant.
+//    char ch;
+//    printf("\nEnter a character: ");
+//    scanf("%c", &ch);
+//     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') 
+//       {printf("%c is vowel", ch);}
+//     else{ printf("%c is consonant", ch);}
+    // EX3:Write c Program to find Largest Among Three Numbers.
+    // float a, b, c;
+    // printf("Enter three numbers: ");
+    // scanf("%f %f %f", &a, &b, &c);
+    // if(a>b && a>c)
+    // {
+    //    printf("%0.3f is the largest number", a);   
+    // }else if(b>a && b>c)
+    // {
+    //    printf("%0.3f is the largest number", b); 
+    // }else
+    // {
+    //    printf("%0.3f is the largest number", c); 
+    // }
+    // EX4:Write C program to check whether a number is positive, negative or zero.
+    // int num;
+    // printf("Enter a number: ");
+    // scanf("%d", &num);
+    // num>0 ? printf("%d is Positive", num) : num<0 ? printf("%d is Negative", num) : printf("%d is Zero", num);
+    //EX5 : Write C program to check whether a Char is alphabet or not.
+//     char ch;
+//    printf("\nEnter a character: ");
+//    scanf("%c", &ch);
+//    if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+//    {
+//       printf("%c is an alphabet", ch);
+//    }else
+//    {
+//       printf("%c is not an alphabet", ch);
+//    }
+    //EX6: Write C program to sum the nutral number from 1.
+    // int num;
+    // int sum = 0;
+    // printf("Enter a number: ");
+    // scanf("%d", &num);
+    // if (num<0 || num==0)
+    // {
+    //     printf("%d is not a positive number", num);
+    // }else
+    // {
+    //     for(int i=1; i<=num; i++)
+    //     {
+    //         sum+=i;
+    //     }
+    // }   
+    // printf("Sum of %d natural numbers is %d", num, sum);
+    //EX7: Write C program to calculate the factorial of a number.
+    // int num, fact=1;
+    // printf("Enter a number: ");
+    // scanf("%d", &num);
+    // if (num==0)
+    //     printf("Factorial of 0 is 1");
+    // if (num<0)
+    // {
+    //     printf("Error !! Factorial of negative number doesn't exist");
+    // }
+    // for(int i=1; i<=num; i++)
+    // {
+    //     fact*=i;
+    // }
+    // printf("Factorial of %d is %d", num, fact);
+    //EX8: Write C program to make a calculator. 
+    // int num1, num2;
+    // char choice;
+    // printf("Enter two numbers: ");
+    // scanf("%d %d", &num1, &num2);
+    // printf("Enter an operator: ");
+    // scanf(" %c", &choice);
+    // switch(choice)
+    // {
+    //     case '+': printf("%d + %d = %d", num1, num2, num1+num2);
+    //     break;
+    //     case '-': printf("%d - %d = %d", num1, num2, num1-num2);
+    //     break;
+    //     case '*': printf("%d * %d = %d", num1, num2, num1*num2);
+    //     break;
+    //     case '/': printf("%d / %d = %d", num1, num2, num1/num2);
+    //     default: printf("Error!! Wrong Operator");
+    // }
+    return 0;    
+}
